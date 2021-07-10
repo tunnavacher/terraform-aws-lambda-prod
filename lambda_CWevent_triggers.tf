b6c08f2d1be91773f5b71c1e8d891f8201d88bc1
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-07-11 00:01:19 +0530

    Update lambda_CWevent_triggers.tf

--- lambda_CWevent_triggers.tf
+++ lambda_CWevent_triggers.tf
@@ -15,7 +15,7 @@ resource "aws_cloudwatch_event_rule" "lambda_trigger_after_crawler_succeded" {
 	],
 	"detail": {
 		"crawlerName": [
-			"GlueJOblambdatest1"
+			"s3crawler"
 		],
 		"state": [
 			"Succeeded"
